e875927410907e368a7c9a585419ef303cadc498
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-30 19:22:54 -0400

    sistema pessoa

---
+++ sistemapessoa/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace sistemapessoa
+{
+    public class Pessoa
+    {
+        private string nome;
+
+        public void setNome(string nome){
+            this.nome = nome;
+        } 
+
+        public string getNome(){
+            return this.nome;
+        }
+
+        public Pessoa()
+        {
+        }
+    }
+}
---
+++ sistemapessoa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sistemapessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Informe seu nome: ");
+            pessoa.setNome(Console.ReadLine());
+
+            Console.WriteLine("\n\nOla " + pessoa.getNome());
+        }
+    }
+}
